797eb5f7ca532de612a7e4deed1283931b6c95d5
Dicedead <dicedead@gmail.com>
2024-03-18 10:37:56 +0100

    [xray] reordering

---
+++ src/pyxu/experimental/xray/2dtests/bunny_lcav_method/bunny_comparisons.py
@@ -0,0 +1,270 @@
+from dataclasses import dataclass
+
+import imageio.v3 as iio
+import matplotlib.pyplot as plt
+import numpy as np
+import skimage
+
+import pyxu.abc as pxa
+import pyxu.experimental.xray._xray as xray
+import pyxu.info.ptype as pxt
+import pyxu.operator.linop.base as pxlb
+import pyxu.runtime as pxrt
+import pyxu.util as pxu
+from pyxu.abc import DiffMap, ProxFunc
+from pyxu.operator import PositiveOrthant, SquaredL2Norm
+from pyxu.opt.solver import PGD
+from pyxu.opt.stop import RelError
+
+
+class SigLU(pxa.DiffMap):
+    def __init__(self, dim: pxt.Integer):
+        super().__init__(shape=(dim, dim))
+        self.lipschitz = 1 / 4
+        self.diff_lipschitz = 1 / (6 * np.sqrt(3))
+
+    @pxrt.enforce_precision(i="arr")
+    def apply(self, arr: pxt.NDArray) -> pxt.NDArray:
+        xp = pxu.get_array_module(arr)
+        x = -arr
+        xp.exp(x, out=x)
+        x += 1
+        x = 1 / x
+        x[arr < 0] = 0.25 * arr[arr < 0] + 0.5
+        return x
+
+    @pxrt.enforce_precision(i="arr", o=False)
+    def jacobian(self, arr: pxt.NDArray) -> pxt.OpT:
+        x = self.apply(arr)
+        v = x.copy()
+        x -= 1
+        v *= x
+        v[arr < 0] = 0.25
+        return pxlb.DiagonalOp(v)
+
+
+@dataclass
+class ReconstructionTechnique:
+    ground_truth: pxt.NDArray
+    op: DiffMap
+    regularizer: ProxFunc
+    initialisation: pxt.NDArray
+    diff_lip: float
+
+    def run(self, stop_crit=RelError(eps=1e-3), post_process_optres=None):
+        data_fidelity = SquaredL2Norm(np.prod(self.ground_truth.shape)).argshift(-self.ground_truth.flatten()) * self.op
+        pgd = PGD(data_fidelity, self.regularizer)
+        pgd.fit(x0=self.initialisation, stop_crit=stop_crit, track_objective=True, tau=1 / self.diff_lip)
+        alpha, _ = pgd.stats()
+        alpha = alpha["x"]
+
+        if post_process_optres is not None:
+            alpha = post_process_optres(alpha)
+
+        return self.op(alpha).reshape(*self.ground_truth.shape)
+
+
+side = 512
+gamma = 0.3
+beta = 15
+num_n = 1000
+num_t = 350
+lambda_ = 4
+transmittance_ratio = 0.6  # I_center = trans_ratio * I_0
+
+
+def disk(side=side, radius=side / 4):
+    x, y = np.meshgrid(np.linspace(-side / 2, side / 2, side), np.linspace(-side / 2, side / 2, side))
+    ground_truth = 1 * (x**2 + y**2 <= (radius**2))
+    return ground_truth
+
+
+def ellipsis(side_a, num_a, side_b, num_b):
+    side_a = side_a / 2
+    side_b = side_b / 2
+    x, y = np.meshgrid(np.linspace(-side_a, side_a, num_a), np.linspace(-side_b, side_b, num_b))
+    ground_truth = 1 * ((x / side_a) ** 2 + (y / side_b) ** 2 <= 1)
+    return ground_truth
+
+
+def absorption_coeff(sides, ratio=transmittance_ratio):
+    sides = pitch * sides
+    return -np.log(ratio) * np.sum(sides) / (2 * np.prod(sides))
+
+
+def inverse_absorption_map(sides, ratio=transmittance_ratio):
+    sides = pitch * sides
+    alpha = absorption_coeff(sides, ratio)
+    distance_max = sides[0] / 2
+    points = np.indices(sides.astype(int)).transpose((1, 2, 0)) * pitch
+    dist = np.linalg.norm(points - distance_max, axis=-1)
+    distances = distance_max - dist
+    correction_map = np.exp(alpha * distances)
+    return correction_map
+
+
+def chirp_signal(sides, a, T=1):
+    # sin = np.sin(2 * np.pi * np.linspace(0, v_max, num=sides[0]) * np.linspace(0, T, num=sides[0]))
+    sin = np.sin(2 * np.pi * a * (np.linspace(0, T, num=sides[0]) ** 2))
+    sin = np.sign(sin).clip(0, 1)
+    sin = np.tile(sin.reshape(1, -1), (sides[1], 1))
+    return sin
+
+
+def concentric_circles(path="../res/concentric.png", downsample_x=2, downsample_y=2, inverted=False):
+    im = iio.imread(path)[::downsample_y, ::downsample_x, 0]
+    if inverted:
+        im = im.max() - im
+    return im / im.max()
+
+
+def bunny_high():
+    return 1 * iio.imread("../../3dtests/pngs/bunny_zres_100_reweighted__088.png")
+
+
+def bunny_middle1():
+    return 1 * iio.imread("../../3dtests/pngs/bunny_zres_100_reweighted__056.png")
+
+
+def bunny_middle2():
+    return 1 * iio.imread("../../3dtests/pngs/bunny_zres_100_reweighted__033.png")
+
+
+def bunny_low():
+    return 1 * iio.imread("../../3dtests/pngs/bunny_zres_100_reweighted__009.png")
+
+
+ground_truth = bunny_high()  # chirp_signal([side, side], a=20)  # epfl_logo(inverted=True)
+
+side = np.array(ground_truth.shape)
+origin = 0
+pitch = np.array([1.0, 1.0])
+w_spec = absorption_coeff(side) * ellipsis(pitch[1] * side[1], side[1], pitch[0] * side[0], side[0])
+
+angle = np.linspace(0, 2 * np.pi, num_n, endpoint=False)
+t_max = pitch * side / 2  # 10% over ball radius
+t_offset = np.linspace(-t_max[0], t_max[1], num_t, endpoint=True)
+
+n = np.stack([np.cos(angle), np.sin(angle)], axis=1)
+t = n[:, [1, 0]] * np.r_[-1, 1]  # <n, t> = 0
+
+n_spec = np.broadcast_to(n.reshape(num_n, 1, 2), (num_n, num_t, 2))  # (N_angle, N_offset, 2)
+t_spec = t.reshape(num_n, 1, 2) * t_offset.reshape(num_t, 1)
+t_spec += pitch * side / 2
+
+weighted_xrt = xray.XRayTransform.init(
+    arg_shape=ground_truth.shape,
+    origin=origin,
+    pitch=pitch,
+    method="ray-trace",
+    n_spec=n_spec.reshape(-1, 2),
+    t_spec=t_spec.reshape(-1, 2),
+    w_spec=w_spec,
+)
+
+unweighted_xrt = xray.XRayTransform.init(
+    arg_shape=ground_truth.shape,
+    origin=origin,
+    pitch=pitch,
+    method="ray-trace",
+    n_spec=n_spec.reshape(-1, 2),
+    t_spec=t_spec.reshape(-1, 2),
+    w_spec=None,
+)
+
+
+lcav_low = ReconstructionTechnique(
+    ground_truth=bunny_low(),
+    op=weighted_xrt.T,
+    regularizer=PositiveOrthant(weighted_xrt.codim),
+    initialisation=0 * np.random.randn(weighted_xrt.codim),
+    diff_lip=400000,
+)
+
+lcav_high = ReconstructionTechnique(
+    ground_truth=bunny_high(),
+    op=weighted_xrt.T,
+    regularizer=PositiveOrthant(weighted_xrt.codim),
+    initialisation=0 * np.random.randn(weighted_xrt.codim),
+    diff_lip=400000,
+)
+
+lcav_middle1 = ReconstructionTechnique(
+    ground_truth=bunny_middle1(),
+    op=weighted_xrt.T,
+    regularizer=PositiveOrthant(weighted_xrt.codim),
+    initialisation=0 * np.random.randn(weighted_xrt.codim),
+    diff_lip=400000,
+)
+
+lcav_middle2 = ReconstructionTechnique(
+    ground_truth=bunny_middle2(),
+    op=weighted_xrt.T,
+    regularizer=PositiveOrthant(weighted_xrt.codim),
+    initialisation=0 * np.random.randn(weighted_xrt.codim),
+    diff_lip=400000,
+)
+
+
+def plot_four_images(imgs, subtitles, main_title, file_title, processing=None):
+    if processing is None:
+        processing = [lambda x: x] * 4
+
+    fig, ax = plt.subplots(nrows=2, ncols=2)
+    ax_flat = ax.flatten()
+    fig.suptitle(main_title, fontsize=16)
+    fig.subplots_adjust(hspace=0.5)
+
+    for idx, a in enumerate(ax_flat):
+        im = a.imshow(processing[idx](imgs[idx]), cmap="Greys", vmin=0, vmax=1)
+        a.set_title(subtitles[idx])
+        plt.colorbar(im, ax=a)
+
+    fig.savefig(file_title, dpi=500)
+    plt.close(fig)
+
+
+def threshold_processing(image):
+    otsu = skimage.filters.threshold_otsu(image, nbins=2)
+    print(otsu)
+    res = image.copy()
+    res[image < otsu] = 0
+    res[image >= otsu] = 1
+    return res
+
+
+if __name__ == "__main__":
+    lcav_low_img = lcav_low.run()
+    lcav_middle1_img = lcav_middle1.run()
+    lcav_middle2_img = lcav_middle2.run()
+    lcav_high_img = lcav_high.run()
+
+    plot_four_images(
+        [bunny_low(), lcav_low_img, bunny_high(), lcav_high_img],
+        ["GT low", "Low", "GT high", "High"],
+        "No thresholding",
+        "no_thresholding_part1.png",
+    )
+
+    plot_four_images(
+        [bunny_low(), lcav_low_img, bunny_high(), lcav_high_img],
+        ["GT low", "Low", "GT high", "High"],
+        "With thresholding",
+        "with_thresholding_part1.png",
+        processing=[lambda x: x, threshold_processing, lambda x: x, threshold_processing],
+    )
+
+    plot_four_images(
+        [bunny_middle1(), lcav_middle1_img, bunny_middle2(), lcav_middle2_img],
+        ["GT mid 1", "Mid 1", "GT mid 2", "Mid 2"],
+        "No thresholding",
+        "no_thresholding_part2.png",
+    )
+
+    plot_four_images(
+        [bunny_middle1(), lcav_middle1_img, bunny_middle2(), lcav_middle2_img],
+        ["GT mid 1", "Mid 1", "GT mid 2", "Mid 2"],
+        "With thresholding",
+        "with_thresholding_part2.png",
+        processing=[lambda x: x, threshold_processing, lambda x: x, threshold_processing],
+    )
---
+++ src/pyxu/experimental/xray/3dtests/refraction.py
@@ -0,0 +1,230 @@
+import numpy as np
+
+import pyxu.info.ptype as pxt
+import pyxu.util.array_module as pxa
+
+
+def normalize(k: pxt.NDArray) -> pxt.NDArray:
+    """
+    Normalize an array of vectors.
+
+    Parameters
+    ----------
+    k: (N,D)
+
+    Returns
+    -------
+    Normalized (N,D) version of k, along each D dimensional vector line.
+
+    """
+    return k / np.linalg.norm(k, axis=-1)[:, np.newaxis]
+
+
+def zero_z(k: pxt.NDArray) -> pxt.NDArray:
+    """
+    Zero out last array dimension
+
+    Parameters
+    ----------
+    k: (N,D)
+
+    Returns
+    -------
+    k with a 0 component in its last dimension
+
+    """
+    q = k.copy()
+    q[:, -1] = 0
+    return q
+
+
+def snell_vectorial(n1: pxt.Real, n2: pxt.Real, k_incident: pxt.NDArray, n_normal: pxt.NDArray) -> pxt.NDArray:
+    """
+    Compute refracted ray.
+
+    Note:
+    If n1 > n2 total reflectance may happen for sufficiently oblique angles.
+    The resulting refracted ray is a nan vector, since that ray effectively does not exist.
+
+    Parameters
+    ----------
+    n1: refractive index of incoming ray's medium
+    n2: refractive index of refracted ray's medium
+    k_incident: normalized incident ray (N, 3)
+    n_normal: normalized normal ray (N, 3)
+
+    Returns
+    -------
+    Refracted rays for each (k_incident, n_normal) pair, (N, 3)
+
+    """
+    xp = pxa.get_array_module(k_incident)
+    ratio = n1 / n2
+    first_term = ratio * xp.cross(n_normal, xp.cross(-n_normal, k_incident, axis=-1), axis=-1)
+    inner = 1 - (first_term**2).sum(axis=-1)
+    mask = inner < 0
+    inner[mask] = 0.0
+    second_term = (-n_normal) * xp.sqrt(inner)[:, xp.newaxis]
+    temp = first_term + second_term
+    temp[mask] = xp.nan
+    return temp
+
+
+def intersection_line_sphere(
+    radius: pxt.Real, height: pxt.NDArray, t_init: pxt.NDArray, n_dir: pxt.NDArray
+) -> pxt.NDArray:
+    """
+    Compute (if any) the position of an intersection of a ray with a cylinder.
+
+    Parameters
+    ----------
+    radius: (1,) cylinder radius
+    height: (2,) min and max cylinder heights
+    t_init: (N,3) position of ray outside cylinder
+    n_dir: (N,3) normalized direction of ray outside cylinder
+
+    Returns
+    -------
+    Position of intersection with the sphere, (N,3)
+    If no intersection: returns the nan vector.
+
+    """
+    xp = pxa.get_array_module(t_init)
+
+    t_init_2 = t_init[:, :2]
+    n_dir_2 = n_dir[:, :2]
+    dot_prod = xp.einsum("ij,ij->i", t_init_2, n_dir_2)
+
+    radicand = (dot_prod**2) + radius * radius - (t_init_2**2).sum(axis=-1)
+
+    no_intersec_mask = radicand < 0 | xp.any(xp.isnan(n_dir), axis=-1)
+    radicand[no_intersec_mask] = 0.0
+
+    sqrt_rad = xp.sqrt(radicand)
+    x1 = -dot_prod + sqrt_rad
+    x2 = -dot_prod - sqrt_rad
+
+    v1 = t_init + n_dir * x1[:, xp.newaxis]
+    v2 = t_init + n_dir * x2[:, xp.newaxis]
+
+    t_out = v1
+    v2_mask = xp.linalg.norm(v1 - t_init, axis=-1) > xp.linalg.norm(v2 - t_init, axis=-1)
+    tmp = v2[v2_mask]
+    t_out[v2_mask] = tmp
+
+    t_out[no_intersec_mask] = xp.nan
+    t_out[(t_out[:, -1] > height[1]) | (t_out[:, -1] < height[0])] = xp.nan
+    return t_out
+
+
+def intersection_polymer(c_spec: pxt.NDArray, t_in: pxt.NDArray, n_refracted: pxt.NDArray) -> pxt.NDArray:
+    """
+    Compute (if any) the position of an intersection of a ray with the polymer.
+
+    Parameters
+    ----------
+    c_spec: (2,) glass thickness and cylinder external diameter
+    t_in: (N,3) position on cylinder border
+    n_refracted: (N,3) normalized direction inside cylinder (as in inside the glass)
+
+    Returns
+    -------
+    Position of intersection with the polymer, (N,3)
+    If no intersection: returns the nan vector.
+
+    """
+    inner_radius = c_spec[1] / 2 - c_spec[0]
+    return intersection_line_sphere(inner_radius, c_spec[-2:], t_in, n_refracted)
+
+
+def intersection_cylinder(c_spec: pxt.NDArray, t_init: pxt.NDArray, n_in: pxt.NDArray) -> pxt.NDArray:
+    """
+    Compute (if any) the position of an intersection of a ray with the polymer.
+
+    Parameters
+    ----------
+    c_spec: (2,) glass thickness and cylinder external diameter.
+    t_init: (N,3) initial position outside cylinder.
+    n_in: (N,3) normalized direction outside cylinder
+
+    Returns
+    -------
+    Position of intersection with the cylinder, (N,3)
+    If no intersection: returns the nan vector.
+
+    """
+    outer_radius = c_spec[1] / 2
+    return intersection_line_sphere(outer_radius, c_spec[-4:-2], t_init, n_in)
+
+
+def f(n_in: pxt.NDArray, t_init: pxt.NDArray, r_spec: pxt.NDArray, c_spec: pxt.NDArray) -> (pxt.NDArray, pxt.NDArray):
+    """
+
+    Parameters
+    ----------
+    n_in: (N,3) ray direction outside cylinder (not necessarily normalized)
+    t_init: (N,3) ray position outside cylinder
+    r_spec: (3,) index of refraction of air/cylinder/jelly
+    c_spec: (6,) glass thickness, cylinder external diameter, glass min height, glass max height, polymer min height,
+                polymer max height
+
+    Returns
+    -------
+    (n_out, t_out): ray direction/position inside cylinder, ((N,3), (N,3))
+    Both vectors are nan-full if incoming ray does not enter jelly
+
+    """
+    n_in = normalize(n_in)
+
+    xp = pxa.get_array_module(n_in)
+    n_air, n_cyl, n_gel = r_spec[0], r_spec[1], r_spec[2]
+    t_in = intersection_cylinder(c_spec, t_init, n_in)
+    n_refracted = snell_vectorial(n_air, n_cyl, n_in, normalize(zero_z(t_in)))
+    t_out = intersection_polymer(c_spec, t_in, n_refracted)
+    n_out = snell_vectorial(n_cyl, n_gel, n_refracted, normalize(zero_z(t_out)))
+
+    nan_mask = xp.any(xp.isnan(t_out), axis=-1)
+    n_out[nan_mask] = xp.nan
+    return n_out, t_out
+
+
+def sine(v, w):
+    sines = np.zeros(len(v))
+    for i in range(len(v)):
+        sines[i] = np.sqrt(1 - (v[i] @ w[i]) ** 2)
+
+    return sines
+
+
+def test_snell():
+    k = np.array([[1, -1, 0], [-1, -4, -5], [-1, -2, 6], [5, 3, 2]])
+    k = k / np.linalg.norm(k, axis=-1)[:, np.newaxis]
+    k = k
+    n = np.array([[0, 1, 0], [1, 5, 1], [1, 2, 9], [5, 1, 4]])
+    n = n / np.linalg.norm(n, axis=-1)[:, np.newaxis]
+    n1 = 1.9
+    n2 = 1
+    res1 = snell_vectorial(n1, n2, k, n)
+
+    print(snell_vectorial(n1, n2, k, n))
+    print(k)
+    print(np.linalg.norm(res1, axis=-1))
+    print(np.linalg.norm(k, axis=-1))
+    print(n2 * sine(res1, n) - n1 * sine(k, n))
+
+
+def test_f():
+    c_spec = [1, 8, 0, 10, 0, 10]
+    r_spec = [1, 1.4, 1.45]
+    n_in = np.array([[-1, 0, 0], [-1, 0.4, 0.1]])
+
+    t_init = np.array([[20, 0, 5], [-10, 0, 5]])
+
+    n_out, t_out = f(n_in, t_init, r_spec, c_spec)
+
+    print(n_out)
+    print(t_out)
+
+
+if __name__ == "__main__":
+    test_f()
