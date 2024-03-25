e20c2a317593203303cbbcf75d01bf8bcd61a496
Dicedead <dicedead@gmail.com>
2024-03-25 09:15:33 +0100

    [xray] revert init

--- src/pyxu/operator/linop/__init__.py
+++ src/pyxu/operator/linop/__init__.py
@@ -1,5 +1,3 @@
 from pyxu.info.plugin import _load_entry_points
-from pyxu.operator.linop.xrt import *
-from pyxu.operator.linop.xrt.ray import *
 
 _load_entry_points(globals(), group="pyxu.operator.linop")
