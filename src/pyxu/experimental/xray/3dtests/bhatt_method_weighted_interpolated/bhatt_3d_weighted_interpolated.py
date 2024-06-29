e77c4cfd99c6f42bb962208cf183fead57f8469b
Dicedead <dicedead@gmail.com>
2024-06-29 18:25:03 +0200

    [xray] bunny not weighted

--- src/pyxu/experimental/xray/3dtests/bhatt_method_weighted_interpolated/bhatt_3d_weighted_interpolated.py
+++ src/pyxu/experimental/xray/3dtests/bhatt_method_weighted_interpolated/bhatt_3d_weighted_interpolated.py
@@ -140,13 +140,13 @@ def bunny_padded(path="../npys/bunny_zres_150_padded.npy"):
 print("Loading ground truth...")
 optimize_save = True
 refraction = False
-weighted = True
+weighted = False
 z_weights = True
 gpu = True
 gpu = gpu and optimize_save
 xp = cp if gpu else np
-ground_truth = benchy_padded()
-chosen_gt = "benchy_padded"
+ground_truth = bunny_padded()
+chosen_gt = "bunny_padded"
 ground_truth = ground_truth if not gpu else cp.array(ground_truth)
 chosen_gt = chosen_gt + "_weighted" if weighted else chosen_gt
 chosen_gt = chosen_gt + "_refracted" if refraction else chosen_gt
